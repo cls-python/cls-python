4d687d4050eee5e62b6b333c73d0838c07900e8e
Jan Bessai <Jan.Bessai@tu-dortmund.de>
2022-03-09 18:35:50 +0100

    Add linebreaks in deep_str

--- cls_python/debug_util.py
+++ cls_python/debug_util.py
@@ -2,18 +2,18 @@ from typing import Iterable
 
 
 def deep_str(obj) -> str:
+    sep = ",\n"
     if isinstance(obj, list):
-        return f"[{','.join(map(deep_str, obj))}]"
+        return f"[{sep.join(map(deep_str, obj))}]"
     elif isinstance(obj, dict):
-        sep = ",\n"
         return f"map([{sep.join(map(lambda kv: ':'.join([deep_str(kv[0]), deep_str(kv[1])]) , obj.items()))}])"
     elif isinstance(obj, set):
-        return f"set([{','.join(map(deep_str, obj))}])"
+        return f"set([{sep.join(map(deep_str, obj))}])"
     elif isinstance(obj, tuple):
-        return f"({','.join(map(deep_str, obj))})"
+        return f"({sep.join(map(deep_str, obj))})"
     elif isinstance(obj, str):
         return obj
     elif isinstance(obj, Iterable):
-        return f"iter([{','.join(map(deep_str, obj))}])"
+        return f"iter([{sep.join(map(deep_str, obj))}])"
     else:
         return str(obj)
